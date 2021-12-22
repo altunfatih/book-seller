d46614ffcfb3091dc11c27ff48ec8c60d855aa92
Fatih <fatih.altun00@hotmail.com>
2021-12-22 15:51:08 +0300

    entity classes created

---
+++ src/main/java/com/altunfatih/bookseller/model/Book.java
@@ -0,0 +1,37 @@
+package com.altunfatih.bookseller.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "title", nullable = false, length = 100)
+	private String title;
+	
+	@Column(name = "description", nullable = false, length = 100)
+	private String description;
+	
+	@Column(name = "author", nullable = false, length = 100)
+	private String author;
+	
+	@Column(name = "price", nullable = false)
+	private Double price;
+	
+	@Column(name = "create_time", nullable = false)
+	private LocalDateTime createTime;
+}
---
+++ src/main/java/com/altunfatih/bookseller/model/User.java
@@ -0,0 +1,40 @@
+package com.altunfatih.bookseller.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "username", unique = true, nullable = false, length = 100)
+	private String username;
+	
+	@Column(name = "password", nullable = false, length = 100)
+	private String password;
+	
+	@Column(name = "name", nullable = false, length = 100)
+	private String name;
+	
+	@Column(name = "create_time", nullable = false)
+	private LocalDateTime createTime;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false)
+	private Role role;
+}
